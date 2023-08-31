6bee348bb6bc016cf994bc5973a8a511da6df5c8
dfralan <80047722+dfralan@users.noreply.github.com>
2023-08-31 08:53:57 -0300

    Update newBoard.js

--- tripwire/src/js/core/newBoard.js
+++ tripwire/src/js/core/newBoard.js
@@ -8,6 +8,10 @@ const newBoardInputDescription = document.getElementById('newBoardInputDescripti
 const newBoardInputTags = document.getElementById('newBoardInputTags')
 // here go mentions but for now only private boards ()
 const boardModalIndicator = document.getElementById('boardModalIndicator')
+// Deadline
+const datePickerDD = document.getElementById('datePickerDD');
+const timePickerDD = document.getElementById('timePickerDD');
+
 var workspaceHash = '' 
 var newBoardHash = ''
 var existentBoardLS = ''
@@ -49,6 +53,8 @@ function launchModalBoard(boardId) {
         newBoardSubmitButton.textContent = 'Create Board'
         boardRevisionsAmount = '-1'
         boardEventHash = genHex(20)
+        datePickerDD.value = `${''}-${''}-${''}`;
+        timePickerDD.value = `${''}:${''}`;
 
     } else { // Existent board edition
 
@@ -63,6 +69,8 @@ function launchModalBoard(boardId) {
         newBoardSubmitButton.textContent = 'Confirm'
         boardRevisionsAmount = existentBoardLS[8]
         boardEventHash = existentBoardLS[9]
+        //datePickerDD.value = getDateTimeFromTimestamp(existentBoardLS[5])[0]
+        //timePickerDD.value = getDateTimeFromTimestamp(existentBoardLS[5])[1]
     }
 
     showNewBoardModal()
@@ -76,6 +84,12 @@ newBoardForm.addEventListener('submit', function (event) {
     const newBoardName = newBoardNameInput.value
     const newBoardDescription = newBoardInputDescription.value
     const newBoardTags = newBoardInputTags.value
+
+    // deadline values
+    //const selectedDate = datePickerDD.value;
+    //const selectedTime = timePickerDD.value;
+    //const combinedDateTime = new Date(`${selectedDate}T${selectedTime}`);
+    //const boardDeadline = combinedDateTime.getTime();
 
     //0 workspaceId, 1 boardId, 2 title, 3 tags, 4 deadline, 5 at, 6 participants
     const newBoardArrayed = [
